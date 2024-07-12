2ac89ba89fa39e5fd8c6610baab4b5d3cda0bb5b
LugpDev2022 <lugpdev2022@gmail.com>
2024-07-12 13:02:54 -0600

    feat: darle las instrucciones al modelo de ai para convertirse en un asistente de meditacion

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,5 @@
 import { openai } from '@ai-sdk/openai';
-import { streamText } from 'ai';
+import { StreamingTextResponse, streamText, StreamData } from 'ai';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -10,7 +10,27 @@ export async function POST(req: Request) {
   const result = await streamText({
     model: openai('gpt-4-turbo'),
     messages,
+    system: `\
+    Eres un asistente de meditación que creará una rutina de meditación guiada según lo que el usuario necesite.
+    Deberás hacerle una pregunta a la vez al usuario sobre lo siguiente:
+    1. ¿Cual es su estado de animo?
+    2. ¿Cuanto tiempo quieres dedicar?
+    3. ¿Por qué quieres meditar?
+
+    Posteriormente genera la rutina, pero no le des al usuario el siguiente paso hasta que te indique que ya terminó el anterior.
+
+    Al finalizar la rutina envia un mensaje de felicitación y da por finalizada la sesión.
+    `,
+  });
+  const data = new StreamData();
+
+  data.append({ test: 'value' });
+
+  const stream = result.toAIStream({
+    onFinal(_) {
+      data.close();
+    },
   });
 
-  return result.toAIStreamResponse();
+  return new StreamingTextResponse(stream, {}, data);
 }
